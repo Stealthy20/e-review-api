53d2cdd6cdf80a62dc6b0d8ded0d9f7bf5625e53
Stealthy20 <kjellin.fredrik@live.se>
2023-02-06 07:54:26 +0000

    Add create signal to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -7,7 +8,6 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_l8ywve'
     )
@@ -17,3 +17,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
