ad9319967063188c4469331d62f36a0563365f45
Stealthy20 <kjellin.fredrik@live.se>
2023-02-20 08:45:54 +0000

    Change comments in reviews

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,10 @@ from keep.models import Keep
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Profile model
+    The create method handles the reviews count.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     reviews_count = serializers.ReadOnlyField()
@@ -11,7 +15,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
 
     class Meta:
         model = Profile
